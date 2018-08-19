af1e9730ae7866dcfd5fb0cd53e89f26cf2ee072
me@john.fitness <Jetpilot1986^>
2018-08-19 22:39:32 +1000

    Begin command line args

--- source/ciri.c
+++ source/ciri.c
@@ -6,14 +6,71 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <argtable3.h>
 
 #include "utils/logger_helper.h"
 #include "server/server.h"
 
 #define MAIN_LOGGER_ID "main"
 
+struct arg_lit *help, *version;
+struct arg_file *config;
+struct arg_str *dlog;
+struct arg_end *end;
 
 int main(int argc,char *argv[]) {
+
+    if (LOGGER_VERSION != logger_version()) {
+        return EXIT_FAILURE;
+    }
+    logger_init();
+    logger_output_register(stdout);
+    logger_output_level_set(stdout, LOGGER_DEBUG);
+    logger_helper_init(MAIN_LOGGER_ID, LOGGER_DEBUG, true);
+
+    void *argtable[] = {
+            help    = arg_lit0(NULL, "help", "display this help and exit"),
+            version = arg_lit0(NULL, "version", "display version info and exit"),
+            config  = arg_file0("c", "config", "config.json", "ciri configuration"),
+            dlog    = arg_strn("D", NULL, "LOGGER_ENABLE",0,argc+2,"definitions"),
+            end     = arg_end(20),
+    };
+
+    int exitcode = 0;
+    char progname[] = "ciri";
+
+    int nerrors;
+    nerrors = arg_parse(argc,argv,argtable);
+
+    log_info(MAIN_LOGGER_ID, "Initializing %s\n", progname);
+
+    log_info(MAIN_LOGGER_ID, "Config file: %s\n", config->filename[0]);
+
+    /* special case: '--help' takes precedence over error reporting */
+    if (help->count > 0)
+    {
+        printf("Usage: %s", progname);
+        arg_print_syntax(stdout, argtable, "\n");
+        printf("List information about the FILE(s) "
+               "(the current directory by default).\n\n");
+        arg_print_glossary(stdout, argtable, "  %-25s %s\n");
+        exitcode = 0;
+        goto exit;
+    }
+
+    /* If the parser returned any errors then display them and exit */
+    if (nerrors > 0)
+    {
+        /* Display the error details contained in the arg_end struct.*/
+        arg_print_errors(stdout, end, progname);
+        printf("Try '%s --help' for more information.\n", progname);
+        exitcode = 1;
+        goto exit;
+    }
+
+    exit:
+    /* deallocate each non-null entry in argtable[] */
+    arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
 
     struct json_object *jobj;
     char *str = "{ \"msg-type\": [ \"0xdeadbeef\", \"irc log\" ], \
@@ -31,16 +88,6 @@ int main(int argc,char *argv[]) {
 
     jobj = json_tokener_parse(str);
     printf("jobj from str:\n---\n%s\n---\n", json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY));
-
-    if (LOGGER_VERSION != logger_version()) {
-        return EXIT_FAILURE;
-    }
-    logger_init();
-    logger_output_register(stdout);
-    logger_output_level_set(stdout, LOGGER_DEBUG);
-    logger_helper_init(MAIN_LOGGER_ID, LOGGER_DEBUG, true);
-
-    log_info(MAIN_LOGGER_ID, "Initializing cIRI core\n");
 
     int rc;
     MDB_env *env;
