8c413328906b999e7e0707c9449e368640f717bb
John Cottrell <johnc@businesscraft.com.au>
2018-08-21 10:28:17 +1000

    test gitingore for submodules

--- cmake/Findargtable3.cmake
+++ cmake/Findargtable3.cmake
@@ -1,4 +1,5 @@
 set(ARGTABLE3_DEPS_DIR ${CMAKE_SOURCE_DIR}/deps/argtable3)
+configure_file(cmake/in/gitignore.in ${ARGTABLE3_DEPS_DIR}/.gitignore COPYONLY)
 configure_file(cmake/in/argtable3.in ${ARGTABLE3_DEPS_DIR}/CMakeLists.txt COPYONLY)
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" . WORKING_DIRECTORY ${ARGTABLE3_DEPS_DIR})
 execute_process(COMMAND ${CMAKE_COMMAND} --build . WORKING_DIRECTORY ${ARGTABLE3_DEPS_DIR})
